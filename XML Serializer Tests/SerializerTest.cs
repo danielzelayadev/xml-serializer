733a88e5c05a74b8ff49b7ed32020158b29eb169
Wupa9 <wupaZ9@gmail.com>
2016-03-30 01:37:05 -0600

    Number serialization done

--- XML Serializer Tests/SerializerTest.cs
+++ XML Serializer Tests/SerializerTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using XML_Serializer;
 
 namespace XML_Serializer_Tests
@@ -17,17 +18,43 @@ namespace XML_Serializer_Tests
         [TestMethod]
         public void Serialize_Integer()
         {
+            Serialize_Number(new[] { 100, 50, 10, -10, 2, 0, 1000, 100000, -1000, -2 });
+        }
+
+        [TestMethod]
+        public void Serialize_Long()
+        {
+            Serialize_Number(new[] { 100000000000000L, 50000000000000L, 1000000000000000000L,
+                                     -10000000000000L, -20000000000000000L });
+        }
+
+        [TestMethod]
+        public void Serialize_Float()
+        {
+            Serialize_Number(new[] { 100.50f, 50.22f, 10.452f, -10.2031f, 2.393393f, 
+                                     0.5f, 1000.39438f, 100000.1f, -1000.453f });
+        }
+
+        [TestMethod]
+        public void Serialize_Double()
+        {
+            Serialize_Number(new[] { 10001.10010110011, 1.39848498392, 
+                                     -45.384482938383838, 20.299293111 });
+        }
+
+        private void Serialize_Number<T>(IEnumerable<T> testValues)
+        {
             var serializer = new XMLSerializer();
 
-            int[] testInts = new[] { 100, 50, 10 -10, 2, 0, 1000, 100000, -1000, -2 };
-
-            foreach (var testInt in testInts)
+            foreach (var testVal in testValues)
             {
-                string result = serializer.Serialize(testInt);
+                var result = serializer.Serialize(testVal);
 
-                Assert.AreEqual("<int>"+testInt+"</int>", result, "Should return the correct xml representation.");
+                Assert.AreEqual("<num>" + testVal + "</num>", result, 
+                                "Should return the correct xml representation.");
             }
         }
+
 
     }
 }
