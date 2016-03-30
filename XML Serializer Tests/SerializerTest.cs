9c1f2fa7eba6bc2e5fb993ba9c69f52d440f9cee
Wupa9 <wupaZ9@gmail.com>
2016-03-29 23:12:23 -0600

    Add tests for serializer

---
+++ XML Serializer Tests/SerializerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XML_Serializer;
+
+namespace XML_Serializer_Tests
+{
+    [TestClass]
+    public class SerializerTest
+    {
+        [TestMethod]
+        public void Serializer_Init ()
+        {
+            var serializer = new XMLSerializer();
+
+            Assert.IsNotNull(serializer, "The Serializer should not be null when initialized");
+        }
+
+        [TestMethod]
+        public void Serialize_Integer()
+        {
+            var serializer = new XMLSerializer();
+
+            int[] testInts = new[] { 100, 50, 10 -10, 2, 0, 1000, 100000, -1000, -2 };
+
+            foreach (var testInt in testInts)
+            {
+                string result = serializer.Serialize(testInt);
+
+                Assert.AreEqual("<int>"+testInt+"</int>", result, "Should return the correct xml representation.");
+            }
+        }
+
+    }
+}
